8c5a9c7937231b2c5edee5a24a12783ebd56b96c
Darshan Shankar <d.n.shankar@gmail.com>
2013-09-26 10:47:54 -0700

    new

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ btn.setBackgroundColor(Color(0.45,0.75,0.83,1));
 btn.setTitle('Unlock New Level'); 
 controller.view.addSubview(btn);
 
+
+
+
+
 var ct2 = ViewController();
 ct2.view.setBackgroundColor(Color(1,1,1,1));
 
@@ -13,13 +17,16 @@ var lbl = Label(134, 125, 300, 80);
 lbl.setText('More levels with new skills and scenarios');
 ct2.view.addSubview(lbl);
 var btn2a = Button(100, 225, 150, 80); 
-btn2a.setBackgroundColor(Color(0.45,0.75,0.83,1)); 
+btn2a.setBackgroundColor(Color(0.2,0.8,0.2,1)); 
 btn2a.setTitle('Take a survey'); 
-controller.view.addSubview(btn2a);
+ct2.view.addSubview(btn2a);
 var btn2b = Button(318, 225, 150, 80); 
 btn2b.setBackgroundColor(Color(0.45,0.75,0.83,1)); 
 btn2b.setTitle('$0.99'); 
-controller.view.addSubview(btn2b);
+ct2.view.addSubview(btn2b);
+
+
+
 
 function buttonPress(sender){
   if(sender.currentTitle == 'Unlock New Level'){
